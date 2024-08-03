2d2a3c7c8e26083fe935713b07030ef63d9d6ad2
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-08-03 12:38:01 +0300

    Вес що на уроці

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ print(df.groupby("SibSp")['Survived'].mean())
 print(df.groupby("Parch")['Survived'].mean())
 print(df.groupby("Embarked")['Survived'].mean())
 
-df.drop(['PassengerId','Name','Ticket','Cabin'],axis=1,inplace=True)
 
 df.info()
 
@@ -22,9 +21,9 @@ def fill_sex(data):
 df["Sex"] = df['Sex'].apply(fill_sex)
 df.info()
 
-age1 = df[df['Pclass'] == '1']['Age'].median()
-age2 = df[df['Pclass'] == '2']['Age'].median()
-age3 = df[df['Pclass'] == '3']['Age'].median()
+age1 = df[df['Pclass'] == 1]['Age'].median()
+age2 = df[df['Pclass'] == 2]['Age'].median()
+age3 = df[df['Pclass'] == 3]['Age'].median()
 
 def fill_age(row):
     if pd.isnull(row['Age']):
@@ -36,8 +35,26 @@ def fill_age(row):
             return age3
     return row['Age']
 
-df['Age'] = df.apply(fill_age,axis=1)
+df['Age'] = df.apply(fill_age, axis=1)
 
 df.info()
 print(df['Embarked'].value_counts())
 df['Embarked'].fillna('S',inplace=True)
+df.info()
+
+df[list(pd.get_dummies(df['Embarked']).columns)]=pd.get_dummies(df['Embarked'])
+df.info()
+
+
+df.drop(['PassengerId','Name','Ticket','Cabin','Embarked'],axis=1,inplace=True)
+df.info()
+
+def is_alone(data):
+    if data['SibSp']+ data['Parch'] == 0:
+        return 1
+    return 0 
+
+df['Alone'] = df.apply(is_alone, axis = 1)
+df.info()
+
+df.to_csv('cleaned_titanic.csv', index=False)
